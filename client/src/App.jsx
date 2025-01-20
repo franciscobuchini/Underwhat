cdd08ec4383eae6cef37fa26e70c0f4f6c5a9ae0
franciscobuchini <franciscobuchini@gmail.com>
2025-01-20 16:00:28 -0300

    starting frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,12 +19,13 @@ function App() {
   }, []);
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-800 text-white'>
+    <div className='flex flex-col justify-center h-screen bg-gray-800 text-white'>
       {products.map((product, index) => (
-        <div key={index} className='text-center p-4 bg-gray-700 rounded-lg m-2'>
+        <div key={index} className='text-left p-4 bg-gray-700 rounded-lg m-2'>
           <p className='text-3xl font-bold'>{product.product_name}</p>
-          <p className='text-xl'>Price: ${product.product_selling.toFixed(2)}</p>
-          <p className='text-lg text-gray-400'>Category: {product.product_category}</p>
+          <p className='text-lg text-gray-400'>{product.product_category}</p>
+          <p className='text-xl'>${product.product_selling.toFixed(2)}</p>
+
         </div>
       ))}
     </div>
