8b1674cee1ce12ce04b0cb0cbea95eed95e0b525
franciscobuchini <franciscobuchini@gmail.com>
2025-01-27 14:06:55 -0300

    adding navbar and cart dropdown menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 //app.jsx
 import ProductList from './components/ProductList';
+import Header from './components/Header';
 import image01 from './assets/01/01.webp';
 
 function App() {
 
   return (
     <>
+      <Header/>
       <ProductList image={image01} />
     </>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,48 @@
 //Header.jsx
+import nav01 from '../assets/Logo/nav01.webp';
+
 function Header() {
 
   return (
-    <div> 
-          
+    <div className="Header">
+      <nav className="Navbar navbar bg-white rounded-2xl shadow">
+        <div className="NavbarLogo flex flex-1 items-center ">
+          <a href="#">
+            <img src={nav01} className="h-8"/>
+          </a>
+        </div>
+        <div className="NavbarMenu navbar-end flex items-center gap-4">
+          <div className="NavbarCart dropdown relative inline-flex [--auto-close:inside] [--offset:8] [--placement:bottom-end]">
+            <button id="dropdown-scrollable" type="button" className="NavbarCartButton dropdown-toggle btn btn-text btn-circle size-10" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+              <div className="CartIndicator indicator">
+                <span className="indicator-item bg-error size-2 rounded-full"></span>
+                <span className="icon-[tabler--shopping-cart] text-gray-400 size-[1.375rem]"></span>
+              </div>
+            </button>
+
+            <div className="DropdownMenu dropdown-menu dropdown-open:opacity-100 hidden rounded-2xl bg-white" role="menu" aria-orientation="vertical" aria-labelledby="dropdown-scrollable">
+              <div className="DropdownHeader dropdown-header justify-center">
+                <h6 className="text-gray-600">Cart</h6>
+              </div>
+              <div className="DropdownCart vertical-scrollbar rounded-scrollbar text-gray-600 overflow-auto max-md:max-w-60">
+                <div className="ProductAdded dropdown-item rounded-2xl">
+                  <div className="ProductImage">
+                    <img className="rounded-2xl h-20 object-cover" src="https://cdn.flyonui.com/fy-assets/avatar/avatar-1.png" />
+                  </div>
+                  <div>
+                    <h6 className="ProductName text-gray-600">Product Name</h6>
+                    <p className="ProductSize text-gray-400"> <small>Product Size</small> </p>
+                    <p className="ProductPrice text-gray-400"> <small>Product Price</small> </p>
+                    <a className="ProductDelete text-red-400"> <small>Delete</small> </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
+    
   );
 }
 
