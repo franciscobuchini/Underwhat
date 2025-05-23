60f58d6b64a5f29b4f203056441c1e06c3530a31
franciscobuchini <franciscobuchini@gmail.com>
2025-05-23 22:08:01 +0200

    new component added: productGallery

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -13,7 +13,7 @@ export default function ProductGallery({ images }) {
   const displayImages = images && images.length > 0 ? images : defaultImages;
 
   return (
-    <div className="p-2 grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2">
       {displayImages.map((url, idx) => (
         <div
           key={idx}
