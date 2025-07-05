02cbe760a6d6376d85825a280458a638b6460e2f
franciscobuchini <franciscobuchini@gmail.com>
2025-07-05 23:44:03 +0200

    countries moved to countries.js

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -4,6 +4,7 @@ import { Icon } from '@iconify/react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../components/CartContext';
 import emailjs from '@emailjs/browser';
+import countries from '../data/Countries';
 
 const CheckoutForm = ({
   couponCode = "",
@@ -16,10 +17,6 @@ const CheckoutForm = ({
   const [formValid, setFormValid] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { cartItems, clearCart } = useCart();
-
-  const handleCountryChange = (event) => {
-    setShowOtherCountry(event.target.value === 'other');
-  };
 
   const checkFormValidity = () => {
     const form = document.querySelector(".needs-validation");
@@ -113,67 +110,20 @@ const CheckoutForm = ({
               <label className="block text-sm font-medium text-gray-600" htmlFor="userCountry">
                 {t("checkout.select_country")} *
               </label>
-              <select
-                id="userCountry"
-                name="userCountry"
-                required
-                onChange={handleCountryChange}
-                defaultValue="united_states"
-                className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-3 text-gray-600 focus:border-pink-800"
-              >
-                <option value="angola">Angola</option>
-                <option value="argentina">Argentina</option>
-                <option value="australia">Australia</option>
-                <option value="bahrain">Bahrain</option>
-                <option value="belgium">Belgium</option>
-                <option value="brazil">Brazil</option>
-                <option value="bulgaria">Bulgaria</option>
-                <option value="canada">Canada</option>
-                <option value="chile">Chile</option>
-                <option value="china">China</option>
-                <option value="colombia">Colombia</option>
-                <option value="croatia">Croatia</option>
-                <option value="czechia">Czechia</option>
-                <option value="denmark">Denmark</option>
-                <option value="ecuador">Ecuador</option>
-                <option value="egypt">Egypt</option>
-                <option value="fiji">Fiji</option>
-                <option value="france">France</option>
-                <option value="germany">Germany</option>
-                <option value="hungary">Hungary</option>
-                <option value="indonesia">Indonesia</option>
-                <option value="ireland">Ireland</option>
-                <option value="israel">Israel</option>
-                <option value="italy">Italy</option>
-                <option value="japan">Japan</option>
-                <option value="jordan">Jordan</option>
-                <option value="kenya">Kenya</option>
-                <option value="liechtenstein">Liechtenstein</option>
-                <option value="malaysia">Malaysia</option>
-                <option value="namibia">Namibia</option>
-                <option value="netherlands">Netherlands</option>
-                <option value="new_caledonia">New Caledonia</option>
-                <option value="new_zealand">New Zealand</option>
-                <option value="norway">Norway</option>
-                <option value="philippines">Philippines</option>
-                <option value="poland">Poland</option>
-                <option value="portugal">Portugal</option>
-                <option value="rwanda">Rwanda</option>
-                <option value="saudi_arabia">Saudi Arabia</option>
-                <option value="serbia">Serbia</option>
-                <option value="singapore">Singapore</option>
-                <option value="slovenia">Slovenia</option>
-                <option value="south_africa">South Africa</option>
-                <option value="korea_south">South Korea</option>
-                <option value="spain">Spain</option>
-                <option value="switzerland">Switzerland</option>
-                <option value="tanzania">Tanzania</option>
-                <option value="turkey">Turkey</option>
-                <option value="united_arab_emirates">United Arab Emirates</option>
-                <option value="united_kingdom">United Kingdom</option>
-                <option value="united_states">United States</option>
-                <option value="other">Other...</option>
-              </select>
+            <select
+              id="userCountry"
+              name="userCountry"
+              required
+              defaultValue="US"
+              className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-3 text-gray-600 focus:border-pink-800"
+            >
+              {countries.map(({ code, label }) => (
+                <option key={code} value={code}>
+                  {label}
+                </option>
+              ))}
+              <option value="OTHER">{t("checkout.other_country_option")}</option>
+            </select>
             </div>
             {showOtherCountry && (
               <div>
@@ -300,7 +250,7 @@ const CheckoutForm = ({
               className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-3 text-gray-600 focus:border-pink-800 resize-none"
             ></textarea>
           </div>
-  
+
           {/* Contact Details */}
           <div className="w-full mt-6">
             <h6 className="text-lg font-bold text-gray-600 flex items-center gap-4">
