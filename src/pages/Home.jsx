7d653fbf81bb9d861bc0c4d138a77f6df235f14b
franciscobuchini <franciscobuchini@gmail.com>
2025-07-18 22:59:40 +0200

    banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,25 @@ import PicGallery from '../components/PicGallery';
 import ProductList from '../components/ProductList';
 import SizesTable from '../components/SizesTable';
 import Title from '../components/Title';
-import { useTranslation } from 'react-i18next';
 import StatsHeader from '../components/StatsHeader';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
   const { t } = useTranslation("global");
+
   return (
-    <div className="container mx-auto sm:px-6 mt-20">
-      <div className="flex flex-col gap-20 my-12">
-        <StatsHeader/>
+  <div className="container mx-auto sm:px-6 mt-10">
+    {/* Banner Image */}
+    <div className="w-full mb-12 relative overflow-hidden rounded-xl shadow-md">
+      <img
+        src="https://res.cloudinary.com/dpleitc1d/image/upload/v1752867653/instagram_post_9_iecucr.webp"
+        alt="Banner"
+        className="w-full object-cover transition-all duration-300 ease-in-out
+                  h-[200px] sm:h-[300px] md:h-[350px] lg:h-[400px] xl:h-[450px]"
+      />
+    </div>
+      <div className="flex flex-col gap-20">
+        {/* <StatsHeader /> */}
         <Title title={t("home.featured_products")} />
         <ProductList />
         <Title title={t("home.table_sizes")} />
@@ -19,8 +29,6 @@ const Home = () => {
         {/* <Title title={"Gallery Pics"} /> */}
         {/* <PicGallery /> */}
       </div>
-      {/* <Title title="The brand for your team" /> */}
-      
     </div>
   );
 };
